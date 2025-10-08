672829ff2216f8e7d21f22d8593b31524c2a09a9
Prabhleen Pawar <112449909+26pawarp@users.noreply.github.com>
2025-10-07 23:13:37 -0400

    Update reverse_args_solution.cpp

--- reverse_args_solution.cpp
+++ reverse_args_solution.cpp
@@ -1,9 +1,5 @@
 #include <iostream>
 int main(int argc, char* argv[]) {
     // Iterate through arguments in reverse order (excluding argv[0])
-    for (int i = argc - 1; i > 0; --i) {
-        std::cout << argv[i] << " ";
-    }
-    std::cout << std::endl;
-    return 0;
+    
 }
